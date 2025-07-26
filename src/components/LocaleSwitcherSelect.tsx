63e136c73f7c87de2ccc4ac5a829b97aec9d2f11
Volynskyi-Kirill <kirill108bol@gmail.com>
2025-07-26 19:15:16 +0200

    refactor: rewrite locale switcher to Select from shadcn

--- src/components/LocaleSwitcherSelect.tsx
+++ src/components/LocaleSwitcherSelect.tsx
@@ -1,10 +1,15 @@
 'use client';
 
-import clsx from 'clsx';
 import { useParams } from 'next/navigation';
 import { Locale } from 'next-intl';
-import { ChangeEvent, ReactNode, useTransition } from 'react';
+import { ReactNode, useTransition } from 'react';
 import { usePathname, useRouter } from '@/i18n/navigation';
+import {
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 type Props = {
   children: ReactNode;
@@ -22,43 +27,28 @@ export default function LocaleSwitcherSelect({
   const pathname = usePathname();
   const params = useParams();
 
-  function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
-    const nextLocale = event.target.value as Locale;
+  function onSelectChange(nextLocale: string) {
     startTransition(() => {
       router.replace(
         // @ts-expect-error -- TypeScript will validate that only known `params`
         // are used in combination with a given `pathname`. Since the two will
         // always match for the current route, we can skip runtime checks.
         { pathname, params },
-        { locale: nextLocale }
+        { locale: nextLocale as Locale }
       );
     });
   }
 
   return (
-    <label
-      className={clsx(
-        'relative text-muted-foreground',
-        isPending && 'transition-opacity opacity-50 cursor-not-allowed'
-      )}
+    <Select
+      defaultValue={defaultValue}
+      onValueChange={onSelectChange}
+      disabled={isPending}
     >
-      <p className='sr-only'>{label}</p>
-      <select
-        className={clsx(
-          'inline-flex w-full appearance-none rounded-md bg-input text-foreground',
-          'py-2 pl-3 pr-8 text-sm shadow-sm',
-          'border border-border focus:outline-none focus:ring-2 focus:ring-ring',
-          isPending && 'opacity-50 cursor-not-allowed'
-        )}
-        defaultValue={defaultValue}
-        disabled={isPending}
-        onChange={onSelectChange}
-      >
-        {children}
-      </select>
-      <span className='absolute right-2 top-2 pointer-events-none text-foreground'>
-        ⌄
-      </span>
-    </label>
+      <SelectTrigger>
+        <SelectValue placeholder={label} />
+      </SelectTrigger>
+      <SelectContent>{children}</SelectContent>
+    </Select>
   );
 }
